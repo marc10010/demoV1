8fb939759c2b0ae8baf8d9543c45d9cd4af1f4c2
marc10010 <75836335+marc10010@users.noreply.github.com>
2024-03-03 18:25:50 +0100

    simplyfy vieews

--- test1/views.py
+++ test1/views.py
@@ -8,12 +8,3 @@ from rest_framework import status
 def index(request):
     return Response({"message": "Hello, world. You're at the test1 index."}, status= status.HTTP_200_OK)
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
